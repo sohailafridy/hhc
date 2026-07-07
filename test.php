a72b28a7200647a0b1d001e66d3e9d3c623a50fb
sohailafridy <85743945+sohailafridy@users.noreply.github.com>
2026-07-07 15:49:23 +0500

    First commit
    first

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+	include('config.php');
+
+
+
+
+
+
+	// $q = "INSERT INTO camps
+	// 	set
+
+	// 	camp_name ='test',
+	// 	date   = '2021-20-20'
+	// ";
+
+	// mysqli_query($con,$q);
+
+
+
+
+
+
+?>
